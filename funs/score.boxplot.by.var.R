ced75463b765910246eb40b91ec53880dc8f526b
Franch Exposito <franches@lski2816.mskcc.org>
2020-01-08 11:12:28 -0500

    Adding new 'score.boxplot.by.var.R', with density plots when GLOBAL scores and vivid colors

--- funs/score.boxplot.by.var.R
+++ funs/score.boxplot.by.var.R
@@ -1,3 +1,11 @@
+############
+# Proposta LAIA #
+############
+
+### Score.boxplot.by.var.R ###
+
+# score <- "GCS"
+# data <- ff_box_score
 
 score.boxplot.by.var <- function (score, data) {
   
@@ -15,20 +23,20 @@ score.boxplot.by.var <- function (score, data) {
     totem_order_by_score <- names(summary(totem))
     
     if (score == "BCS") {
-      palette_score_alpha <- colorRampPalette(c("deepskyblue", "deepskyblue4"), alpha=TRUE)(length(sub_vars))
+      palette_score_alpha <- colorRampPalette(c("deepskyblue1", "royalblue3"), alpha=TRUE)(length(sub_vars)) ##he canviat els blaus
     } else if (score == "FCS") {
-      palette_score_alpha <- colorRampPalette(c("darkorange", "darkorange4"), alpha=TRUE)(length(sub_vars))
+      palette_score_alpha <- colorRampPalette(c("orange1", "orangered2"), alpha=TRUE)(length(sub_vars)) ##he canviat els taronges
     } else if (score == "GCS") {
-      palette_score_alpha <- colorRampPalette(c("darkkhaki", "darkolivegreen"), alpha=TRUE)(length(sub_vars))
+      palette_score_alpha <- colorRampPalette(c("limegreen", "springgreen4"), alpha=TRUE)(length(sub_vars)) ##he canviat els verds
     }
     
     # p <- ggplot(data, aes(x=reorder(get(var), get(score), FUN=median),
     #                       y=get(score), fill=get(var))) +
     p <- ggplot(data, aes(x=get(var), y=get(score), fill=get(var)))+
-      stat_boxplot(geom ='errorbar', width=0.2, color="gray40") +
+      stat_boxplot(geom ='errorbar', width=0.2, color="black") +    ##canvi gray40 to black
       # geom_violin(color="gray40", aes(fill=get(var)), trim=FALSE) +
-      geom_boxplot(color="gray40", aes(fill=get(var))) +
-      scale_fill_manual(values=setNames(palette_score_alpha, totem_order_by_score)) +
+      geom_boxplot(color="black", size = 1, width = 0.6, aes(fill=get(var))) +   ##canvi gray40 to black and add size / width parameters
+      scale_fill_manual(var, values=setNames(palette_score_alpha, totem_order_by_score)) +
       xlab(var) + ylab(score)
     
     real_groups <- sub_vars
@@ -49,7 +57,7 @@ score.boxplot.by.var <- function (score, data) {
         values1 <- data[which(data[,var]==name1),score]
         values2 <- data[which(data[,var]==name2),score]   
         
-        ans_test <- t.test(values1,values2)
+        ans_test <- wilcox.test(values1,values2) # Wilcox-test
         p_value <- round(ans_test$p.value,3)
         p_value
       })))
@@ -71,38 +79,53 @@ score.boxplot.by.var <- function (score, data) {
         p_vals_here[i] <- val2
       }
       
-      p <- p + geom_signif(comparisons = e_list, map_signif_level=FALSE, step_increase=0.06 , annotation=p_vals_here, textsize = 5)
+      p <- p + geom_signif(comparisons = e_list, map_signif_level=FALSE, step_increase=0.06 , annotation=p_vals_here, textsize = 5.5)
       
     }
     
     
   } else if (ncol(data)==3) {
     if (score == "BCS") {
-      color <- "deepskyblue4"
+      color <- "deepskyblue1"          ##canvi de color (com el que he posat a dalt)
+      color_line <- "royalblue3"
     } else if (score == "FCS") {
-      color <- "darkorange4"
+      color <- "orange1"          ##canvi de color (com el que he posat a dalt)
+      color_line <- "orangered2"
     } else if (score == "GCS") {
-      color <- "darkolivegreen"
+      color <- "limegreen"        ##canvi de color (com el que he posat a dalt)   
+      color_line <- "springgreen4"
     }
-    p <- ggplot(data, aes(x="", y=get(score))) +
-      stat_boxplot(geom ='errorbar', width=0.2, color="gray40") +
-      # geom_violin(color="gray40", fill=color, trim=FALSE) +      
-      geom_boxplot(color="gray40", fill=color) +
-      xlab("Global") + ylab(score)
+    # p <- ggplot(data, aes(x="", y=get(score))) +
+    #   stat_boxplot(geom ='errorbar', width=0.2, color="black") + ##canvi gray40 a black
+    #   # geom_violin(color="gray40", fill=color, trim=FALSE) +      
+    #   geom_boxplot(color="gray40", fill=color,  size = 1, width = 0.6) +   ##afegit size and width
+    #   xlab("Global") + ylab(score)
+    v_median <- median(data[,score])
+    
+    p <- ggplot(data, aes(x=get(score))) +
+      geom_density(color="black", size = 1, width = 0.6, fill=color)+
+      geom_vline(xintercept = v_median, color=color_line, linetype="dashed", size=0.6)+
+      theme(legend.position="none")+
+      labs(x=paste(score, "values"))+
+      theme(text = element_text(size=40),
+            legend.position=c(0.8,0.8),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line(colour = "black"))
+    
     
   }
-  
-  
-  
-  
-  p2 <- p + theme_classic()+
-    theme(text = element_text(size=14),
-          # axis.title.y= element_text(margin=margin(t=0,r=10,b=0,l=0)),
-          # axis.title.x= element_text(margin=margin(t=15,r=0,b=0,l=0)),
-          axis.text.x = element_text(size=12, angle=45, hjust=1),
-          plot.margin = unit(c(1,1,1,1), "cm"),
-          legend.position="none")
 
+  
+  #new version
+  p2 <- p + theme_minimal()                  ##canvi de theme
+  theme(axis.text.x = element_text(colour="black", size = 14, face='bold'), ##canvi linies / lletra
+        axis.text.y = element_text(colour="black", size = 14), 
+        axis.title.y = element_text(size = 14, face = 'bold.italic'),
+        legend.position="none")
+  
+  
   
   p2
   
